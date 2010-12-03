8d8056c426a02520b6b3ffcc4d3b1449e6493075
webPragmatist <chris@webPragmatist.com>
2010-12-03 12:00:20 -0600

    Adding assets

---
+++ 960gsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bundle\960gsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+* 960gs Bundle
+*
+* @author Chris LeBlanc <chris@webPragmatist.com>
+*/
+class 960gsBundle extends Bundle
+{
+}
